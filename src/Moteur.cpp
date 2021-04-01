418f7d546c4d276f96f543ce96a75d3cd3bbd15b
Pierrick MADE <pierrick.made@epita.fr>
2021-04-01 14:20:56 +0200

    ADD - Moteur.cpp - tests nice scene
    Signed-off-by: Pierrick MADE <pierrick.made@epita.fr>

--- src/Moteur.cpp
+++ src/Moteur.cpp
@@ -266,9 +266,78 @@ void polygon() {
   image.save_as_ppm("images/polygon.ppm");
 }
 
+void create_polygon_walls_in_scene(Scene& scene) {
+
+  Caracteristics caracteristics_blue(Pixel(0, 0, 255), 0.8, 0, 1);
+  const std::string filename = "images/wood.ppm";
+
+  std::vector<int> faceIndex = {4};
+  std::vector<int> vertexIndices = {0,1,2,3};
+  std::vector<Point3> points = {{4,-1,-1}, {4, -1, 1}, {4, 1, 1}, {4, 1, -1}};
+  std::vector<Vector3> normals = {{-1,0,0}, {-1,0,0}, {-1,0,0}, {-1,0,0}};
+  std::vector<Point3> textureCoordinates = {{0,0,0}, {1,0,0}, {1,1,0}, {0,1,0}};
+  triangleMesh(scene, std::make_shared<Image_Texture>(caracteristics_blue, filename), faceIndex, vertexIndices
+               , points, normals, textureCoordinates);
+  points = {{2,-1,-1}, {2, -1, 1}, {4,-1, 1}, {4, -1, -1}};
+  normals = {{0,1,0}, {0,1,0}, {0,1,0}, {0,1,0}};
+  textureCoordinates = {{0,0,0}, {1,0,0}, {1,1,0}, {0,1,0}};
+  triangleMesh(scene, std::make_shared<Image_Texture>(caracteristics_blue, filename), faceIndex, vertexIndices
+               , points, normals, textureCoordinates);
+  points = {{4,-1,-1}, {2, -1, -1}, {2, 1, -1}, {4, 1, -1}};
+  normals = {{0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}};
+  textureCoordinates = {{0,0,0}, {1,0,0}, {1,1,0}, {0,1,0}};
+  triangleMesh(scene, std::make_shared<Image_Texture>(caracteristics_blue, filename), faceIndex, vertexIndices
+               , points, normals, textureCoordinates);
+}
+
+void create_polygon_water_in_scene(Scene& scene) {
+
+  Caracteristics caracteristics_blue(Pixel(0, 0, 255), 0.8, 0, 0, 0.8);
+
+  std::vector<int> faceIndex = {4};
+  std::vector<int> vertexIndices = {0,1,2,3};
+  std::vector<Point3> points = {{4,-1, 0}, {2, -1, 0}, {2, 1, 0}, {4, 1, 0}};
+  std::vector<Vector3> normals = {{0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}};
+  std::vector<Point3> textureCoordinates = {{0,0,0}, {1,0,0}, {1,1,0}, {0,1,0}};
+  triangleMesh(scene, std::make_shared<Uniform_Texture>(caracteristics_blue), faceIndex, vertexIndices
+               , points, normals, textureCoordinates);
+}
+
+void pool() {
+  Point3 center(0, 0, 2);
+  Point3 spotted_point(4, 0, 0);
+  Vector3 up(1, 0, 2);
+  float alpha = 45.0;
+  float beta = 45.0;
+  float zmin = 1.0; //Why changing this parameter does not affect the output image
+  Camera camera(center, spotted_point, up, alpha, beta, zmin);
+  Scene scene = Scene(camera, 5);
+  scene.msaa_samples = 4;
+  scene.set_epsilon(0.001);
+  Caracteristics caracteristics_green(Pixel(0, 255, 0), 0.1, 0.3, 1);
+  Caracteristics caracteristics_yellow(Pixel(255, 255, 0), 1, 0, 0);
+  auto ground = std::make_shared<Plane>(std::make_shared<Uniform_Texture>(caracteristics_green),
+                                        Point3(0, 0, -1.1), Vector3(0, 0, 1));
+
+  auto light = std::make_shared<Point_Light>(Point3(2, 0, 0), 1000);
+  auto light2 = std::make_shared<Point_Light>(Point3(0, 0, 10), 500);
+  auto sphere = std::make_shared<Sphere>(std::make_shared<Procedural_Texture>(caracteristics_yellow), Point3(3,0,-0.75), 0.25);
+  scene.add_light(light);
+  scene.add_light(light2);
+  scene.add_object({ground, sphere});
+
+
+  create_polygon_walls_in_scene(scene);
+  create_polygon_water_in_scene(scene);
+  std::cout << scene.raycast(Rayon(Vector3(4,0,0.98), center), scene.max_bounces);
+  Image image = scene.raycasting();
+  image.save_as_ppm("images/pool.ppm");
+}
+
 //TODO change the two planes in refraction test
 int main() {
-  polygon();
+  pool();
+  //polygon();
   //refraction_sphere_on_plane();
   //blob_test();
   //triangle_on_plane();
